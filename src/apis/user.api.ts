917b3c05fd35f4a0bfa00d7b73be536d5edf7c3d
thanhgithub123@gmail.com <thanhgithub123@gmail.com>
2024-06-02 16:31:26 +0700

    Mới dựng xong API cho Profile

---
+++ src/apis/user.api.ts
@@ -0,0 +1,29 @@
+import { User } from 'src/types/user.type'
+import { SuccessResponse } from 'src/types/utils.type'
+import http from 'src/utils/http'
+
+interface BodyUpdateProfile extends Omit<User, '_id' | 'email' | 'roles' | 'createdAt' | 'updatedAt'> {
+  password?: string
+  newPassword?: string
+}
+
+const userApi = {
+  getProfile() {
+    return http.get<SuccessResponse<User>>('me')
+  },
+
+  updateProfile(body: BodyUpdateProfile) {
+    return http.put<SuccessResponse<User>>('user', body)
+  },
+
+  uploadAvatar(body: FormData) {
+    return http.post<SuccessResponse<string>>('users/upload-avatar', body, {
+      //   truyền ảnhb thìta phải cấu hình headers như vầy
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+  }
+}
+
+export default userApi
